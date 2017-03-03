92262b4beb7459e17c7e1fbe6bf09453dd836302
periberry <you092579@gmail.com>
2017-03-03 10:12:24 +0900

    prot and sword complete

---
+++ src/app/weapon.ts
@@ -0,0 +1,8 @@
+export class Weapon {
+  name_weapon: string;
+  name_photonart: string;
+  damage: number;
+  consumption: number;
+  frames: number;
+}
+
---
+++ src/app/weaponspec.ts
@@ -0,0 +1,9 @@
+export class Weaponspec {
+  atk1_rest: number;
+  atk1_frames: number;
+  atk2_rest: number;
+  atk2_frames: number;
+  atk3_rest: number;
+  atk3_frames: number;
+  reg: number;
+}
